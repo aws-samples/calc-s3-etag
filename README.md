e702b69ad061e3ff7dc77dedef7f3e058bf4720a
David Byte <14841925+dmbyte@users.noreply.github.com>
2021-05-14 15:18:49 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
-## My Project
+# calc-s3-etag
+When a file is uploaded to S3 or Snowball Edge it has an md5 sum calculated and stored as an etag.  For larger files that are uploaded using multi-part uploads, the etag is in the form of [summary hash]-number of parts.  This summary hash does not match the md5sum of the file and thus makes it difficult for users to clearly verify that the source file matches what is in the target location.  
 
-TODO: Fill this README out!
+Using the metadata for a multi-part uploaded file, the chunk size can be roughly determined as it will be about the number of bytes of the object (returned by the s3api head-object command) divided by the number of parts with that result being divided by 1024 * 1024.  
 
-Be sure to:
+This can then be utilized with this script to calculate the summary etag for comparrison with the online object. 
 
-* Change the title in this README
-* Edit your repository description on GitHub
-
-## Security
-
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
-
-## License
-
-This library is licensed under the MIT-0 License. See the LICENSE file.
+Future enhancements that anyone can contribute to this repo are:
+- performance improvements
+- use of getopts to allow for expanded functionality
+- added code to actually get the source object and perform the compare
+- recursive enablement
 
---
+++ calc-s3-etag.py
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import sys
+import hashlib
+
+def calculate_s3_etag(file_path, chunk_size):
+    md5s = []
+
+    with open(file_path, 'rb') as fp:
+        while True:
+            data = fp.read(chunk_size)
+            if not data:
+                break
+            md5s.append(hashlib.md5(data))
+
+    if len(md5s) < 1:
+        return '"{}"'.format(hashlib.md5().hexdigest())
+
+    if len(md5s) == 1:
+        return '"{}"'.format(md5s[0].hexdigest())
+
+    digests = b''.join(m.digest() for m in md5s)
+    digests_md5 = hashlib.md5(digests)
+    return '"{}-{}"'.format(digests_md5.hexdigest(), len(md5s))
+if len(sys.argv) !=3:
+    print("Usage: calc-s3-etag.py {file} {multipart-transfer size}")
+    print("")
+    print("Multipart transfersize may be gotten by referencing the .aws/config file or by using the following example commands to examine a large object uploaded to the SBE")
+    print("aws s3api head-object --bucket {bucketname} --key \"[prefix/]{objectname}\" --endpoint-url {url to SBE device including port}")
+    print("take the resulting content length and divide it by the number after the dash in the Etag.  Take that result and divide it by 1048576")
+    print("the result will be slightly below the multi-part transfer chunk size measured in MB utilized in the transfer")
+mychunksize=int(sys.argv[2]) * 1024 * 1024    
+print(calculate_s3_etag(sys.argv[1],mychunksize))
